5767789edfb2a2ee626d26924b697e3489ab4bed
Dag Frode Solberg <tncditto@gmal.com>
2015-08-29 11:10:37 +0200

    Solved the first assignment

---
+++ assignments/assignment_1/sprengstoff.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+class Kubbe:
+    vekt = None
+    neste = None
+    def __init__(self, vekt):
+        self.vekt = vekt 
+        self.neste = None 
+
+def spor(kubbe):
+    max_vekt = 0
+    while kubbe:
+        if kubbe.vekt > max_vekt:
+            max_vekt = kubbe.vekt
+        kubbe = kubbe.neste
+
+    return max_vekt
+
+# Oppretter lenket liste
+forste = None
+siste = None
+for linje in stdin:
+    forrige_siste = siste
+    siste = Kubbe(int(linje))
+    if forste == None:
+        forste = siste
+    else:
+        forrige_siste.neste = siste
+
+# Kaller loesningsfunksjonen og skriver ut resultatet
+print spor(forste)
